ea2dacd0b46484af0607aaad998f590901a3983b
alessandro.clemente <alessandro.clemente@aorta.com.br>
2010-12-30 14:33:54 -0200

    dffs

---
+++ BranchAlessandro.h
@@ -0,0 +1,16 @@
+//
+//  BranchAlessandro.h
+//  ProdutoA
+//
+//  Created by Alessandro de Figueiredo Clemente on 12/30/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface BranchAlessandro : NSObject {
+
+}
+
+@end
---
+++ BranchAlessandro.m
@@ -0,0 +1,14 @@
+//
+//  BranchAlessandro.m
+//  ProdutoA
+//
+//  Created by Alessandro de Figueiredo Clemente on 12/30/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "BranchAlessandro.h"
+
+
+@implementation BranchAlessandro
+
+@end
