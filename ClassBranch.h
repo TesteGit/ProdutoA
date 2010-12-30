a14301ed4c96f3b93e8db85d907f6dd0041998c8
Claudio <claudio.fernando@aorta.com.br>
2010-12-30 14:28:06 -0200

    asdfa

---
+++ ClassBranch.h
@@ -0,0 +1,16 @@
+//
+//  ClassBranch.h
+//  ProdutoA
+//
+//  Created by claudio.fernando on 30/12/10.
+//  Copyright 2010 Aorta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ClassBranch : NSObject {
+
+}
+
+@end
