3a486d07950645063f56fa3d3099ef99aa680da0
Claudio <claudio.fernando@aorta.com.br>
2010-12-30 10:26:17 -0200

    COMMIT NO BRANCH

---
+++ ControllerBase.h
@@ -0,0 +1,16 @@
+//
+//  ControllerBase.h
+//  ProdutoA
+//
+//  Created by claudio.fernando on 30/12/10.
+//  Copyright 2010 Aorta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ControllerBase : NSObject {
+
+}
+
+@end
---
+++ ControllerBase.m
@@ -0,0 +1,14 @@
+//
+//  ControllerBase.m
+//  ProdutoA
+//
+//  Created by claudio.fernando on 30/12/10.
+//  Copyright 2010 Aorta. All rights reserved.
+//
+
+#import "ControllerBase.h"
+
+
+@implementation ControllerBase
+
+@end
